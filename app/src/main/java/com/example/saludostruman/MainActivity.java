383220ff77177be056ead50af496cb31a9665f75
Edu <118288657+Edux07@users.noreply.github.com>
2023-10-25 21:01:19 +0200

    Todo colocado en la pantalla

--- app/src/main/java/com/example/saludostruman/MainActivity.java
+++ app/src/main/java/com/example/saludostruman/MainActivity.java
@@ -3,10 +3,16 @@ package com.example.saludostruman;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
     CheckBox check, check1, check2;
+    RadioGroup radioGroup;
+    RadioButton radioButtonn1, radioButtonn2, radioButtonn3;
+    Button btn1 , btn2;
 
 
     @Override
@@ -16,5 +22,17 @@ public class MainActivity extends AppCompatActivity {
         check = findViewById(R.id.checkBox4);
         check1 = findViewById(R.id.checkBox5);
         check2= findViewById(R.id.checkBox6);
+        radioButtonn1= findViewById(R.id.radioButton1);
+        radioButtonn2= findViewById(R.id.radioButton2);
+        radioButtonn3= findViewById(R.id.radioButton3);
+        btn1= findViewById(R.id.button);
+        btn2= findViewById(R.id.button2);
+
+    }
+    public void onBtnClic(){
+
+    }
+    public void onBtnClic2(){
+
     }
 }
