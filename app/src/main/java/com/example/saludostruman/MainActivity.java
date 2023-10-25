c5792fc9742dcc08feb25fce2a3e84436fff4428
Edu <118288657+Edux07@users.noreply.github.com>
2023-10-25 20:34:23 +0200

    ..

--- app/src/main/java/com/example/saludostruman/MainActivity.java
+++ app/src/main/java/com/example/saludostruman/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.saludostruman;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
+    CheckBox check, check1, check2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        check = findViewById(R.id.checkBox4);
+        check1 = findViewById(R.id.checkBox5);
+        check2= findViewById(R.id.checkBox6);
     }
 }
