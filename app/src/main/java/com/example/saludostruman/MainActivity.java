b3ed097e2f33ec4991141d9a3e16f4a1aac1dc4b
Edu <118288657+Edux07@users.noreply.github.com>
2023-11-05 08:41:04 +0100

    Primera parte..?

--- app/src/main/java/com/example/saludostruman/MainActivity.java
+++ app/src/main/java/com/example/saludostruman/MainActivity.java
@@ -3,16 +3,18 @@ package com.example.saludostruman;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    CheckBox check, check1, check2;
-    RadioGroup radioGroup;
-    RadioButton radioButtonn1, radioButtonn2, radioButtonn3;
-
+     private CheckBox check, check1, check2;
+    private RadioGroup radioGroup;
+    private RadioButton radioButtonn1, radioButtonn2, radioButtonn3;
+    private TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +26,22 @@ public class MainActivity extends AppCompatActivity {
         radioButtonn1= findViewById(R.id.radioButton1);
         radioButtonn2= findViewById(R.id.radioButton2);
         radioButtonn3= findViewById(R.id.radioButton3);
+        tv1 = findViewById(R.id.tv1);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
+
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == radioButtonn1.getId()) {
+                    tv1.setText("SR");
+                } else if (checkedId == radioButtonn2.getId()) {
+                    tv1.setText("SRTA");
+                }else if(checkedId == radioButtonn3.getId()){
+                    tv1.setText("N/A");
+                }
+            }
+        });
 
 
-    }
-    public void radioButtonn1(){
 
-    }
-    public void radioButton2(){
-
-    }
-    public void radioButton3(){
-
-    }
+}
 }
